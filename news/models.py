da16b5a3e91105a526ff3acf8c296d66afecf59a
Ahmed Emad <ahmed39652003@gmail.com>
2020-06-24 17:51:38 +0200

    Code Refactoring

--- news/models.py
+++ news/models.py
@@ -1,46 +1,7 @@
-import random
-import re
-import string
-
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.text import slugify
 
-
-def rand_slug():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-
-
-def unique_slugify(instance, queryset=None, value=rand_slug(), max_length=255):
-    """function used to give a unique slug to an instance"""
-
-    slug = slugify(value, allow_unicode=True)
-    slug = slug[:max_length]  # limit its len to max_length of slug field
-
-    def _slug_strip(_value):
-        """removes the '-' separator from the end or start of the string"""
-        return re.sub(r'^%s+|%s+$' % ('-', '-'), '', _value)
-
-    slug = _slug_strip(slug)
-    original_slug = slug
-
-    if not queryset:
-        queryset = instance.__class__.objects.all()
-
-    if instance.pk:
-        queryset = queryset.exclude(pk=instance.pk)
-
-    _next = 2
-    while not slug or queryset.filter(slug=slug):
-        slug = original_slug
-        end = '-%s' % _next
-        if len(slug) + len(end) > max_length:
-            slug = slug[:max_length - len(end)]
-            slug = _slug_strip(slug)
-        slug = '%s%s' % (slug, end)
-        _next += 1
-
-    return slug
+from core.utils import unique_slugify
 
 
 class Source(models.Model):
