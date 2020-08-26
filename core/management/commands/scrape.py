a8daf50d72c587e1b8040aebbd72271dff11ed03
Ahmed Emad <ahmed39652003@gmail.com>
2020-08-26 23:21:37 +0200

    Added cronjob, updated requirements.txt

--- core/management/commands/scrape.py
+++ core/management/commands/scrape.py
@@ -5,6 +5,10 @@ from core.news_scraper.scraper import scrapers
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        scrape()
+
+
+def scrape():
         for scraper in scrapers:
             try:
                 scraper.scrape()
